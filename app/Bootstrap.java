3ac4dcc92f91b25c633cf4643fd2e30b77c2dc5f
ciaran <ciaranroche@hotmail.co.uk>
2016-04-11 14:46:32 +0100

    changes to app.conf, check with lab12, changes made to push heroku

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -17,7 +17,7 @@ public class Bootstrap extends Job
   {
     if (User.count() == 0)
     {
-      Fixtures.deleteDatabase();
+      //Fixtures.deleteDatabase();
       Fixtures.loadModels("data.yml");
   
       
--- app/models/User.java
+++ app/models/User.java
@@ -3,6 +3,7 @@ package models;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +12,7 @@ import play.db.jpa.Model;
 import play.db.jpa.Blob;
 
 @Entity
+@Table(name="`User`")
 public class User extends Model
 {
   public String firstName;
