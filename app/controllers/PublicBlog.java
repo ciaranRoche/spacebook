6e57a710ab795cfa6ec51a63aed89a09121becb2
ciaran <ciaranroche@hotmail.co.uk>
2016-04-14 14:51:15 +0100

    Friend link to blog set up

---
+++ app/controllers/PublicBlog.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import java.util.List;
+
+import models.Message;
+import models.Post;
+import models.User;
+import play.Logger;
+import play.mvc.Controller;
+
+public class PublicBlog  extends Controller
+{
+	 public static void visit(Long id)
+	  {
+	    User currentUser = Accounts.getLoggedInUser();
+	    
+	    User user = User.findById(id);
+	    Logger.info("Just visiting the page for " + user.firstName + ' ' + user.lastName);
+	    render(user);
+	  }
+
+}
